27fe4c082d506013a75198ae795e96470cdbe8ef
energy 778 <one.million.dollars@mail.ru>
2020-05-31 17:12:48 +0300

    init commit

---
+++ src/test/java/ru/veretennikov/notificationsystem/NotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.veretennikov.notificationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
